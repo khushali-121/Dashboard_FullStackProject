f7d8487417cf9f594ebdc3517b49e55bef5c92e0
khushali <khushalichauhan6@gmail.com>
2025-12-30 10:24:49 +0530

    changes in app.js origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://ums12.vercel.app",
+      "http://localhost:5173"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
